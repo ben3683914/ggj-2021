79777d9df1e6ca3b2a36c61ae1aa5ddbe246efe0
Ben Mueller <bmueller368@outlook.com>
2021-01-30 17:05:23 -0600

    added more items

--- Assets/Assets/Resources/Scripts/SpriteLibrary.cs
+++ Assets/Assets/Resources/Scripts/SpriteLibrary.cs
@@ -20,23 +20,38 @@ public class SpriteLibrary : MonoBehaviour
 
     private void Awake()
     {
-        //Sprites = new Dictionary<Item.ItemType, Sprite>();
         Items = new Dictionary<Item.ItemType, SpriteDetails>();
         PopulateSprites();
     }
 
     private void PopulateSprites()
     {
+        // none
         Items.Add(Item.ItemType.None, new SpriteDetails()
         {
             Sprite = None,
             PrefabPath = "Prefabs/item.none"
         });
 
+        // dildo
         Items.Add(Item.ItemType.Dildo, new SpriteDetails()
         {
             Sprite = Dildo,
             PrefabPath = "Prefabs/item.dildo"
+        });
+
+        // hammer
+        Items.Add(Item.ItemType.Hammer, new SpriteDetails()
+        {
+            Sprite = Hammer,
+            PrefabPath = "Prefabs/item.hammer"
+        });
+
+        // screwdriver
+        Items.Add(Item.ItemType.Screwdriver, new SpriteDetails()
+        {
+            Sprite = Screwdriver,
+            PrefabPath = "Prefabs/item.screwdriver"
         });
     }
 
